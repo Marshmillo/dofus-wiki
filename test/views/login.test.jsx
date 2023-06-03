4c9864c6019c3afadff49e6ae1c6d72896969e8d
StikW <danieldg10122016@gmail.com>
2023-06-02 20:28:13 -0500

    Creacion login basico

--- test/views/login.test.jsx
+++ test/views/login.test.jsx
@@ -3,13 +3,21 @@ export const login = () => {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  }
+
   return (
-    <form>
+    <>
+    <form onSubmit={handleSubmit}>
       <label for="email">email</label>
-      <input type="email" placeholder="tuemail@gmail.com" id="email" name="email" />
+      <input value={email} type="email" placeholder="tuemail@gmail.com" id="email" name="email" />
       <label for="email">password</label>
-      <input type="password" placeholder="********" id="password" name="password" />
+      <input value={pass} type="password" placeholder="********" id="password" name="password" />
       <button>Log In</button>
     </form>
+    <button>Aún no tienes una cuenta? Registrate</button>
+    </>
   )
 }
