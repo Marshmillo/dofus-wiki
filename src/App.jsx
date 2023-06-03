ffbf685a3d86d5c2abc2e0ee5fc418cb42f63cce
StikW <danieldg10122016@gmail.com>
2023-06-02 21:03:50 -0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import { Login } from '../test/views/login.test'
-import { Register } from '../test/views/register.test'
+import { Login } from './views/login'
+import { Register } from './views/register'
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
